103719a4de0bc45d5a5f48107cc18ee63dc80ad2
Justin Warner <Justin Warner>
2017-01-03 09:13:04 -0800

    initial release

---
+++ GoogleSpellCheck/GoogleSpellCheck/Window1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Collections;
+using Aviad.WPF.Controls;
+using System.Diagnostics;
+
+// Adapted from: http://www.codeproject.com/Articles/44920/A-Reusable-WPF-Autocomplete-TextBox
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            txtbx.Focus();
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e) {
+            Clipboard.SetText(txtbx.Text.Trim());
+        }
+    }
+}
